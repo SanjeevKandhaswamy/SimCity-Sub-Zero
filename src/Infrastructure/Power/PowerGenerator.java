ff3553f31402a57939ed0752d0469c801536fb3f
Lowkik-Sai <lowkiksaipotnuru@gmail.com>
2023-11-28 22:19:42 +0530

    Another try

--- src/Infrastructure/Power/PowerGenerator.java
+++ src/Infrastructure/Power/PowerGenerator.java
@@ -4,6 +4,7 @@ import Infrastructure.InfrastructureElement;
 public class PowerGenerator extends InfrastructureElement {
     private int supply;
 
+    //Lowkik Comment
     public PowerGenerator(String infraID, int level) {
         super(infraID, "PowerGenerator", level);
         this.supply = 30; // Default supply
