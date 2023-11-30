38ac11178eb7ad9f8e86a51c8b10f4bdf86b88a1
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2023-11-30 18:39:39 +0530

    Update Power.java

--- src/Infrastructure/Power/Power.java
+++ src/Infrastructure/Power/Power.java
@@ -10,14 +10,14 @@ public class Power extends InfrastructureElement {
         this.demand = demand;
     }
 
-    // Override displayInfo to include power-specific information
+    // Override displayInfo to include power-specific information.
     @Override
     public void displayInfo() {
         super.displayInfo();
         System.out.println("Power Demand: " + demand + " MW");
     }
 
-    // Function to build power supply
+    // Function to build power supply.
     public void buildPowerSupply() {
         // Implement build logic
         System.out.println("Building Power Supply for: " + getInfraID());
