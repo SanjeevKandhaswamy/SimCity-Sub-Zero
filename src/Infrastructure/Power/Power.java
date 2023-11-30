8dcf6e57fd42ed5400536f3adaeccb7ef11fdedb
Lowkik-Sai <lowkiksaipotnuru@gmail.com>
2023-11-30 18:36:35 +0530

    Added Power.java and code

---
+++ src/Infrastructure/Power/Power.java
@@ -0,0 +1,37 @@
+package Infrastructure.Power;
+
+import Infrastructure.InfrastructureElement;
+
+public class Power extends InfrastructureElement {
+    private int demand;
+
+    public Power(String infraID, int level, int demand) {
+        super(infraID, "Power", level);
+        this.demand = demand;
+    }
+
+    // Override displayInfo to include power-specific information
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Power Demand: " + demand + " MW");
+    }
+
+    // Function to build power supply
+    public void buildPowerSupply() {
+        // Implement build logic
+        System.out.println("Building Power Supply for: " + getInfraID());
+    }
+
+    // Function to destroy power supply
+    public void destroyPowerSupply() {
+        // Implement destroy logic
+        System.out.println("Destroying Power Supply for: " + getInfraID());
+    }
+
+    // Function to check power demand
+    public void checkDemand() {
+        System.out.println("Checking Power Demand for: " + getInfraID());
+        // Implement demand check logic
+    }
+}
--- src/Infrastructure/Power/PowerGenerator.java
+++ src/Infrastructure/Power/PowerGenerator.java
@@ -1,12 +1,24 @@
 package Infrastructure.Power;
 import Infrastructure.InfrastructureElement;
 
-public class PowerGenerator extends InfrastructureElement {
+public class PowerGenerator extends Power {
     private int supply;
 
-    //Lowkik Comment
-    public PowerGenerator(String infraID, int level) {
-        super(infraID, "PowerGenerator", level);
-        this.supply = 30; // Default supply
+    public PowerGenerator(String infraID, int level, int demand, int supply) {
+        super(infraID, level, demand);
+        this.supply = supply;
+    }
+
+    // Override displayInfo to include generator-specific information
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Power Supply: " + supply + " MW");
+    }
+
+    // Function to update power supply
+    public void updateSupply(int newSupply) {
+        this.supply = newSupply;
+        System.out.println("Power Supply Updated to: " + newSupply + " MW");
     }
 }
