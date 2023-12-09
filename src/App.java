3145670e0c8c2f4ded01e284e639a03cf019996d
Tharun Kumarr A <tharunkumarra@gmail.com>
2023-12-10 01:57:33 +0530

    Hashing success!!!

--- src/App.java
+++ src/App.java
@@ -7,7 +7,7 @@ import Services.Park;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int mapSize = 15; // Map size
+        int mapSize = 10; // Map size
         Map cityMap = new Map(mapSize);
         cityMap.initializeMap();
         String[][] map = new String[mapSize][mapSize];
@@ -19,7 +19,6 @@ public class App {
         Point startingPoint = points.getPoint(new Point(2, 2));
         System.out.println("Starting Point: " + startingPoint);
 
-
         // Update the map based on changes made in the buildPark method
         map = cityMap.getMap();
         cityMap.DisplayMap();
@@ -27,5 +26,28 @@ public class App {
         // Create a GamePanel with the updated map
         GamePanel gamePanel = new GamePanel(map);
         gamePanel.displayPanel();
+
+        // Keep checking for the last clicked coordinates in a loop until (0, 0) is clicked
+        while (true) {
+            Point lastClicked = gamePanel.getLastClickedCoordinates();
+            System.out.println("Last clicked coordinates: " + lastClicked);
+
+            // Check if the last clicked coordinates are (0, 0)
+            if (lastClicked.equals(new Point(9, 9))) {
+                System.out.println("Exiting the loop. Coordinates (0, 0) clicked.");
+                break;
+            }
+
+            // Map the last clicked point to its initial point
+            Point initialPoint = points.getPoint(lastClicked);
+            System.out.println("Initial Point for last clicked: " + initialPoint);
+
+            // Add a small delay to avoid consuming too much CPU
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
