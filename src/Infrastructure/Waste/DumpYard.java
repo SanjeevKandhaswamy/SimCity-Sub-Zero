265f254310b61e0d3c713851ad1149c9e394da37
Sanjeevkandhaswamy19032004 <sanjeevkumar1man@gmail.com>
2023-12-12 12:33:28 +0530

    Sewage Plant added

--- src/Infrastructure/Waste/DumpYard.java
+++ src/Infrastructure/Waste/DumpYard.java
@@ -8,4 +8,71 @@ public class DumpYard extends InfrastructureElement {
         super(infraID, "WasteDumpYard", level);
         this.capacity = 80; // Default capacity
     }
+
+    public boolean buildDumpYard() {
+        int side = this.size;
+        String[][] DumpYardStructure = new String[side][side];
+
+        // cheecks whether area is available
+        if(!(gameMAp.isAreaAvailable(location.getX(),location.getY(),side,side))) {
+            return false;
+        }
+
+        for(int i = 0 ; i < DumpYardStructure.length; i++){
+            for(int j = 0 ; j < DumpYardStructure[i].length; j++) {
+                DumpYardStructure[i][j] = " ";
+            }
+        }
+
+        // Filling the Dump yard borders with '+' and the insde with '0x1F22F'
+        int Dump = 0x1F22F ;
+        for(int i = 0; i<=side-1; i++){
+            for(int j = 0; j<=side-1; j++){
+                if(i==0 || i==side-1 || j==0 || j==side-1){
+                    if(i<DumpYardStructure.length && j<DumpYardStructure[i].length){
+                        DumpYardStructure[i][j] = "+" ;
+                    }
+                } else {
+                    DumpYardStructure[i][j] = Character.toString(dump);
+                }
+            }
+        }
+
+        if (gameMap.placeObject(DumpYardStructure,location.getX(),llocation.getY())){
+            return true;
+        }
+        return false;
+    }
+
+    //Override displayInfo to include dump yard specific information
+    @Override
+    public void displayInfo(){
+        super.displayInfo();
+        System.out.println("Dump Yard Capacity: " + this.capacity);
+    }
+
+    // Function to upgrade dump yard
+    public void upgradeDumpYard(int size, int capacity) {
+        this.size += (size > 0) ? size : 20;
+        int status = super.upgradeInfrastructure();
+        if (status == 0) {
+             System.out.println("Not Enough Capital Balance!!");
+        } else if (status == -1) {
+            System.out.println("Dump Yard is already at maximum level..");
+        } else if (buildSewagePlant()) {
+            System.out.println("Dummp Yard is Upgraded :)");
+        } else {
+            System.out.println("Selected area is already occupied!!");
+        } 
+    }
+
+    // Function to destroy dump yard
+    public void destroyDumpYard() {
+        int side = this.size; //Default
+        if (gameMap.destroyObject(side,side, location.getX(),location.getY())) {
+            System.out.println("Dump Yard Destroyed :(");
+        } else {
+            System.out.println("Unable to destroy Dump Yard. Area may be occupied.");
+        }
+    }
 }
--- src/Infrastructure/Waste/SewagePlant.java
+++ src/Infrastructure/Waste/SewagePlant.java
@@ -1,11 +1,86 @@
 package Infrastructure.Waste;
 import Infrastructure.InfrastructureElement;
+import Util.Location;
 
 public class SewagePlant extends InfrastructureElement {
     private int capacity;
 
     public SewagePlant(String infraID, int level) {
         super(infraID, "SewagePlant", level);
-        this.capacity = 70; // Default capacity
+        this.capacity = 70; // Default capacity  
+    }
+
+    public boolean buildSewagePlant() {
+        int side = this.size; //default
+        String[][] SewagePlantStructure = new String[side][side];
+
+        // checks whether area is available
+        if(!(gameMap.isAreaAvailable(location.getX(), location.getY(), side, side))) {
+            return false;
+        }
+
+        for(int i = 0 ; i < SewagePlantStructure.length; i++){
+            for(int j = 0 ; j < SewagePlantStructure[i].length; j++){
+                SewagePlantStructure[i][j] = " ";
+            }
+        }
+
+        // Filling the Sewage plant borders with '+ ' and the inside area with 'u5408'.
+        int sewage = 0x1F33F;
+        for(int i = 0; i<=side -1 ; i++){
+            for(int j = 0 ; j<= side -1 ; j++) {
+                if (i==0 || i == side -1 || j==0 || j==side-1) {
+                    if (i< SewagePlantStructure.length && j < SewagePlantStructure[i].length) {
+                        SewagePlantStructure[i][j] = "+";
+                    }
+                    
+                } else {
+                    SewagePlantStructure[i][j] = Character.toString(sewage);
+                }
+            }
+        }
+
+        if (gameMap.placeObject(SewagePlantStructure,location.getX(),location.getY())) {
+            return true;
+        }
+        return false;
+    }
+
+    //Function to perform regular maintenance
+    public void performMaintenance() {
+        System.out.println(x:"Performing regular maintenance on the Sewage Plant.");
+    
+    }
+
+    //Override displayInfo to include Sewage plant specific information
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Total capacity: " + this.capacity);
+    }
+
+    // Function to upgrade sewage plant
+    public void upgradeSewagePlant(int size, int capacity) {
+        this.size += (size > 0) ? size : 20;
+        int status = super.upgradeInfrastructure();
+        if (status == 0) {
+             System.out.println("Not Enough Capital Balance!!");
+        } else if (status == -1) {
+            System.out.println("Sewage Plant is already at maximum level..");
+        } else if (buildSewagePlant()) {
+            System.out.println("Sewage Plant is Upgraded :)");
+        } else {
+            System.out.println("Selected area is already occupied!!");
+        } 
+    }
+
+    // Function to destroy Sewage plant
+    public void destroySewagePlant() {
+        int side = this.size; //Default
+        if (gameMap.destroyObject(side,side, location.getX(),location.getY())) {
+            System.out.println("Sewage Plant Destroyed :(");
+        } else {
+            System.out.println("Unable to destroy Sewage Plant. Area may be occupied.");
+        }
     }
 }
